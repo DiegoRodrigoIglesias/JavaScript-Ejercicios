830d601329846bbdbe75380c00e6b9376f582c70
Diego Rodrigo Iglesias <rodrigodiegoroman12@gmail.com>
2023-04-16 18:38:14 +0200

    ejercicios 1

---
+++ array.js
@@ -0,0 +1,176 @@
+
+// var comprasSuper = [ "agua", "lechuga", "tomates", "carne", "pescado", "sandia"];
+
+
+// comprasSuper.push ("acelga");
+// comprasSuper.unshift ("naranja");
+// comprasSuper.pop ();
+// comprasSuper.shift();
+
+
+// var pos = comprasSuper.indexOf ("lechuga");
+
+// var sacar = comprasSuper.splice (pos, 1);
+// var elementosDelArray = comprasSuper;
+// var chequeo = comprasSuper.includes ("batata");
+
+
+// console.log (pos);
+// console.log (elementosDelArray);
+// console.log (comprasSuper.length);
+// console.log (chequeo);
+
+// var palabra = "Diegh";
+// var palabraSeparada = palabra.split ("");
+// console.log (palabraSeparada);
+// palabraSeparada.pop ();
+// palabraSeparada. push ("o");
+// var palabraArreglada = palabraSeparada.join ("");
+
+// console.log (palabraSeparada);
+// console.log (palabraArreglada);
+
+// var palabranueva= "zmmx"
+// var palabraBien= palabranueva.split ("");
+// console.log (palabraBien);
+// console.log (palabraBien.length);
+// palabraBien.shift ();
+// palabraBien.unshift ("E");
+// palabraBien.pop ();
+// palabraBien.push("a");
+// var palabraBienbien = palabraBien.join ("");
+// palabraBien.forEach ((palabraBien) => {if ("m" === "m") {console.log ("F")}});
+
+// console.log (palabraBienbien);
+
+// var numeros = [1, 2, 3, 4, 5];
+// numeros.forEach ((num) => console.log (num));
+// numeros.forEach ((num) => {if (num === 3) {console.log (num)}}); 
+// numeros.forEach ((num) => {if (num <=3) {console.log (num)}});
+
+
+// var vegetales =["arroz", "mani", "lechuga", "zanahoria"];
+// console.log (vegetales);
+// console.log (vegetales.length);
+// console.log (vegetales [3]);
+// console.log (vegetales [vegetales.length - 1]);
+
+// var pos= vegetales.indexOf ("lechuga");
+// console.log (pos);
+
+// var sacar= vegetales.splice (pos, 2);
+// console.log (sacar);
+
+// var nuevoArray= sacar.slice ();
+// console.log (nuevoArray);
+
+// console.log (vegetales);
+
+// function encontrarNumero (Number) {
+// var  separado = Number.split ("");
+// console.log (separado);
+// for (let i=0; i < Number.length ; i++) {
+//     if (separado [i] === "7") {
+//         console.log ("hay un 7") }
+//         else {
+//             console.log ("no hubo suerte")
+//         }
+//     } 
+// }
+
+// encontrarNumero ("3423444");
+// encontrarNumero ("5647877");
+
+// var arr = ["dedo", "azul", "arena"];
+// while (arr.length < 5) {
+//    arr.push('Camilo');
+// }
+// console.log(arr);
+
+// var elemento = ["nombre", "calle", "auto"];
+//    console.log (elemento [elemento.length - 1]);
+
+//    var elemento = ["nombre", "calle", "auto"];
+//    console.log (elemento.length);
+//    elemento.push ("avion");
+//    console.log (elemento);
+//    elemento.unshift ("rulo");
+//    console.log (elemento);
+
+//    console.log (elemento [0] + " " + elemento [1]);
+//    elemento.join ("");
+//    console.log (elemento);
+
+//   for (var i=0; i < elemento.length; i++){
+//     if (elemento [i] === "nombre") {
+//     console.log ("true");}
+//     else { console.log ("false")} ;
+// }
+
+// El parámetro "arrayOfNums" debe ser un arreglo de números.
+   // Suma todos los elementos y retorna el resultado.
+   // Tu código:
+
+//    var codigo = [1, 2, 4, 7, 18];
+//    console.log (codigo);
+//    console.log (codigo.length);
+//    var suma = 0;
+//    codigo.forEach (function(num) {
+//     suma -= num;})
+//     console.log (suma);
+
+
+
+// El parámetro "resultadosTest" es un arreglo de números.
+   // Itera (en un bucle) los elementos del arreglo y devuelve el promedio de las notas.
+   // Tu código:
+
+// var resultadosTest = [ 1, 2, 4, 7, 18];
+// var promedio = 0;
+// var cantidad = resultadosTest.length;
+// console.log (cantidad);
+// for (var i=0; i< resultadosTest.length; i++) {
+//     promedio= resultadosTest[i] + promedio;
+// }
+// var resultado= promedio / cantidad;
+// console.log (resultado);
+
+//  // El arreglo recibido por parámetro contiene números.
+//    // Retornar un arreglo con los elementos incrementados en +1.
+//    // Tu código:
+//    var resultadosTest = [ 1, 2, 4, 7, 18];
+//    var suma = 0;
+//    for (i=0; i < resultadosTest.length; i++) {
+//     suma = resultadosTest[i] + 1;
+//     console.log (suma);
+// }
+
+
+// var prueba2 = [3, 4, 5, 7, 8];
+// var prueba22= 0;
+
+// for (var i=0; i < prueba2.length; i++) {
+//    prueba22 = prueba2 [i] + 1;
+//    console.log (prueba22);
+// }
+
+var prueba23 = [3, 4, 5, 7, 8];
+var prueba223= 0;
+
+for (var i=0; i < prueba23.length; i++) {
+   prueba223 = prueba23 [i] + prueba223;
+}
+console.log (prueba223);
+var resultado = prueba223 / prueba23.length;
+console.log (resultado);
+
+
+var suma = [ 3, 4, 10];
+var resultado = 0;
+
+for (var i=0; i < suma.length; i++) {
+   resultado= suma [i] + resultado;
+}
+console.log (resultado);
+
+
